4c332983c99a80e090e73598186dd3c545421bc5
Your Name <you@example.com>
2023-11-17 12:36:25 +0100

    oficiálně dokončil backend (doufám)

--- backend/web/final_concept.py
+++ backend/web/final_concept.py
@@ -18,7 +18,15 @@ def mesec():
     titles = []
 
     for link in links:
-        pretitle = link.split("/")[-2]
+        if "nazory" in link:
+            nlink = link.split("nazory")[0]
+            ind = links.index(link)
+            links.pop(ind)
+            links.insert(ind,nlink)
+
+    for link in links:
+        pretitles = link.split("/")
+        pretitle = max(pretitles,key=len)
         title_list = pretitle.split("-")
         title = ""
         for x in title_list:
@@ -33,7 +41,9 @@ def mesec():
         links.insert(ind,newlink)
     for title,link in zip(titles,links):
         final.append((title,link))
-    return final
+    final = list(set(final))
+    final = [(name, link) for name, link in final if "autori" not in link]
+    return final 
 
 
 
@@ -77,13 +87,9 @@ def businessinfo():
     filtered_links = [link for link in links if "businessinfo.cz/clanky/" in link]
     final_links = filtered_links[2:]
 
-    title = soup.find_all("span",class_="title-text")
-    for x in title:
-        title[title.index(x)] = x
-    pretitles = [t for t in title]
-    final_titles = [x.encode("latin1").decode("utf-8") for x in pretitles]
-    final = [(title,link) for title,link in zip(final_titles,final_links)]
-    return final
+    titles = [max(link.split("/"),key=len).replace("-"," ") for link in final_links]
+    final = [(title,link) for title,link in zip(titles,final_links)]
+    return final  #články a nadpisy se negroupují správně; text se neuhladí (doesnt get polished) správně
 
 def mpo():
     url = "https://www.mpo.cz/"
@@ -105,8 +111,87 @@ def mpo():
     final = [(title,link) for title,link in zip(final_titles,all_links)]
     return final
 
+def kurzy():
+    url = "https://www.kurzy.cz/zpravy/"
+    result = requests.get(url)
+    soup = BeautifulSoup(result.text,"html.parser")
+
+    elem = soup.find("div",class_="hlavnizpravy ecb").find_all("a")
+    links = [link.get("href") for link in elem]
+    titles = [title.text for title in elem]
+    links = links[1:][::2]
+    titles = titles[1:][::2]
+    final = [(title,link) for title,link in zip(titles,links)]
+    return final #funkce display tuto stránku nezobrazuje správně
+
+def display(funcs,articles,num):
+    unit = articles[num]
+    result = requests.get(unit[1])
+    for encoding in ['utf-8', 'iso-8859-1', 'windows-1250', 'cp1250', 'cp1252']:
+            try:
+                # Parse the HTML content using the selected encoding
+                soup = BeautifulSoup(result.content, 'html.parser', from_encoding=encoding)
+                text = soup.find_all(["p","h2"])
+                formatted_text = ":".join(str(element) for element in text)
+                cleaned_text = funcs.clean(formatted_text)
+                #return cleaned_text
+            except Exception as e:
+                continue
+    return cleaned_text
+
+class final_cleaning():
+    def __init__(self,text):
+        self.text = text
+
+    def businessinfo(self):
+        result = self.text.split("BusinessInfo.cz")
+        length = 0
+        output = ""
+        for x in result:
+            if len(x) > 100:
+                output += x
+        return output
+        
+    
+    def mesec_a_podnikatel(self):
+        return self.text.lstrip("[ Zapomenuté heslo nebo jméno , Sdílet")
+    
+    def uctovani(self):
+        return self.text.lstrip("[ Chcete podnikat v oblasti účetnictví? Zveme vás do naší nové Facebookové skupiny, která je určena pro všechny, kteří se touží osamostatnit a stát se tak svobodnou účetní. , ")
+
+    def mpo(self):
+        return self.text.lstrip("[ Přeskočit na: navigaci | hlavní obsah stránky , ")
+    
+    def kurzy(self):
+        text = self.text.split("Další vybrané zprávy »:")
+        return text[1]
+    
+
 if __name__ == "__main__":
     instance = funcs()
-    print(instance.display_article(articles=mpo(),num=1))
+    func = kurzy()
+    num = 1
+    title = func[num][0]
+    text = display(instance,func,num=num)
+    clean = final_cleaning(text)
+    if func == businessinfo():
+        text = clean.businessinfo()
+    elif func == mesec() or func == podnikatel():
+        text = clean.mesec_a_podnikatel()
+    elif func == uctovani():
+        text = clean.uctovani()
+    elif func == mpo():
+        text = clean.mpo()
+    elif func == kurzy():
+        text = clean.kurzy()
+
+    print(title)
+    print("\n")
+    print(text)
+    print("\n")
+    print(func)
+
+
+
 
 
